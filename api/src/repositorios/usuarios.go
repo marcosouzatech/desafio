be58745486f845db22eb092dae893c8fa0fee20d
marcosouzatech <122187312+marcosouzatech@users.noreply.github.com>
2023-07-11 13:49:32 -0300

    Add files via upload

---
+++ api/src/repositorios/usuarios.go
@@ -0,0 +1,115 @@
+package repositorios
+
+import (
+	"api/api/src/modelos"
+	"database/sql"
+	"fmt"
+)
+
+type Usuarios struct {
+	db *sql.DB
+}
+
+// NovoRepositorioDeUsuario cria um reposirio de usuário
+func NovoRepositorioDeUsuarios(db *sql.DB) *Usuarios {
+	return &Usuarios{db}
+}
+
+// criar insere um usuario no banco de dados
+func (repositorio Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
+	statement, erro := repositorio.db.Prepare(
+		"insert into usuarios (nome, nick, email, senha) values(?, ?, ?, ?)",
+	)
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
+	if erro != nil {
+		return 0, erro
+	}
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+	return uint64(ultimoIDInserido), nil
+
+}
+
+// Buscar traz todos usuarios ou traz com base no filtro nome ou nick
+func (repositorio Usuarios) Buscar(nomeOuNick string) ([]modelos.Usuario, error) {
+	nomeOuNick = fmt.Sprintf("%%%s%%", nomeOuNick)
+
+	linhas, erro := repositorio.db.Query(
+		"select id, nome, nick, email, criadoEm from usuarios where nome LIKE ? or nick LIKE ?",
+		nomeOuNick, nomeOuNick,
+	)
+
+	if erro != nil {
+		return nil, erro
+	}
+
+	defer linhas.Close()
+
+	var usuarios []modelos.Usuario
+
+	for linhas.Next() {
+		var usuario modelos.Usuario
+
+		if erro = linhas.Scan(
+			&usuario.ID,
+			&usuario.Nick,
+			&usuario.Nome,
+			&usuario.Email,
+			&usuario.CriadoEm,
+		); erro != nil {
+			return nil, erro
+		}
+		usuarios = append(usuarios, usuario)
+	}
+
+	return usuarios, nil
+}
+
+// BuscaPorID traz um usuario do banco de dados
+func (repositorio Usuarios) BuscarPorID(ID uint64) (modelos.Usuario, error) {
+	linhas, erro := repositorio.db.Query(
+		"select id, nome, nick, email, criadoEm from usuarios where id = ? ",
+		ID,
+	)
+	if erro != nil {
+		return modelos.Usuario{}, erro
+	}
+	defer linhas.Close()
+
+	var usuario modelos.Usuario
+
+	if linhas.Next() {
+		if erro = linhas.Scan(
+			&usuario.ID,
+			&usuario.Nome,
+			&usuario.Nick,
+			&usuario.Email,
+			&usuario.CriadoEm,
+		); erro != nil {
+			return modelos.Usuario{}, erro
+		}
+	}
+	return usuario, nil
+}
+
+func (repositorio Usuarios) Atualizar(ID uint64, usuario modelos.Usuario) error {
+	statement, erro := repositorio.db.Prepare(
+		"update usuarios set nome = ?, nick = ?, email = ?, where id = ?",
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, ID); erro != nil {
+		return erro
+	}
+	return nil
+}
---
+++ api/src/router/rotas/usuarios.go
@@ -0,0 +1,45 @@
+package rotas
+
+import (
+	"api/api/src/controllers"
+	"net/http"
+)
+
+var rotasUsuarios = []Rota{
+	{
+		URI:                "/",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.HealthCheck,
+		RequerAutenticacao: false,
+	},
+	{
+		URI:                "/usuarios",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.CriarUsuario,
+		RequerAutenticacao: false,
+	},
+	{
+		URI:                "/usuarios",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuarios,
+		RequerAutenticacao: false,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuario,
+		RequerAutenticacao: false,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodPut,
+		Funcao:             controllers.AtualizarUsuario,
+		RequerAutenticacao: false,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarUsuario,
+		RequerAutenticacao: false,
+	},
+}
