e3ffc7c69b5781d6bd22529604f3a0311b67ac49
marcosouzatech <122187312+marcosouzatech@users.noreply.github.com>
2023-07-14 21:37:05 -0300

    Update Files

--- main.go
+++ main.go
@@ -6,8 +6,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	newrelic "github.com/newrelic/go-agent/v3/newrelic"
 )
 
+func initNewRelic() {
+	_, err := newrelic.NewApplication(
+		newrelic.ConfigAppName(os.Getenv("APP_NAME")),
+		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
+		newrelic.ConfigDebugLogger(os.Stdout),
+		newrelic.ConfigDistributedTracerEnabled(true),
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+}
 func main() {
 	config.Carregar()
 	fmt.Printf("Escutando na porta %d", config.Porta)
